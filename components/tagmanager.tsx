e449c53a5eeee5c312a95cc48e0c168e188e6cf2
Sinan <sinan_ceviker@yahoo.com>
2024-10-21 11:39:32 +0200

    moving tag manager client side

--- components/tagmanager.tsx
+++ components/tagmanager.tsx
@@ -1,13 +1,18 @@
-import { cookies } from 'next/headers';
+'use client'
+
+import { useEffect, useState } from 'react';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Cookies from 'js-cookie';
 
 export default function GoogleTagManagerInit() {
-    const cookieStore = cookies();
-    const consent = cookieStore.get('cookie_consent')?.value;
+    const [isGTMEnabled, setIsGTMEnabled] = useState(false);
 
-    if (consent === 'accepted') {
-        return <GoogleTagManager gtmId="AW-16731906773" />;
-    }
+    useEffect(() => {
+        const consent = Cookies.get('cookie_consent');
+        if (consent === 'accepted') {
+            setIsGTMEnabled(true);
+        }
+    }, []);
 
-    return null;
+    return isGTMEnabled ? <GoogleTagManager gtmId="AW-16731906773" /> : null;
 }
